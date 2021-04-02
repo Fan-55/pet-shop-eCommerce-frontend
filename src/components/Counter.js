e625de7a8fe0d17428bc707dc131293af58d7021
Fan5 <openconvenience@gmail.com>
2021-04-02 15:18:26 +0800

    modify CartScreen UI

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,55 @@
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addToCart } from '../actions/cartActions'
+import { CART_ACTION_RESET } from '../constants/cartConstants'
 
-export default function Counter({ qty, item }) {
+const Counter = ({ qty, item }) => {
   console.log('Counter Render')
+
+  const [count, setCount] = useState(qty)
 
   const dispatch = useDispatch()
   const increment = () => {
-    item.quantity++
+    setCount(count + 1)
+    item.quantity = count + 1
     dispatch(addToCart(item))
+    dispatch({ type: CART_ACTION_RESET })
   }
   const decrement = () => {
-    if (item.quantity <= 1) return
-    item.quantity--
+    if (count <= 1) return
+    setCount(count - 1)
+    item.quantity = count - 1
     dispatch(addToCart(item))
+    dispatch({ type: CART_ACTION_RESET })
+  }
+  const onChangeHandler = (e) => {
+    // ^[1-9]\d*$ is regExp of positive integer. Ex. 1, 2, ...
+    if (/^[1-9]\d*$/.test(e.target.value)) {
+      setCount(Number(e.target.value))
+      item.quantity = Number(e.target.value)
+      dispatch(addToCart(item))
+      dispatch({ type: CART_ACTION_RESET })
+
+    } else {
+      setCount('')
+      item.quantity = 1
+      dispatch(addToCart(item))
+      dispatch({ type: CART_ACTION_RESET })
+    }
   }
 
   return (
     <div className="counter">
-      <button onClick={decrement}>
-        <i className="fas fa-minus"></i>
-      </button>
-      <span className="qty">{qty}</span>
-      <button onClick={increment}>
-        <i className="fas fa-plus"></i>
-      </button>
+      <i className="fas fa-minus minus" onClick={decrement}></i>
+      <input
+        type="text"
+        value={count}
+        className="qty"
+        onChange={onChangeHandler}
+      />
+      <i className="fas fa-plus plus" onClick={increment}></i>
     </div>
   )
 }
+
+export default Counter
--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Swal from 'sweetalert2'
@@ -5,10 +6,12 @@ import Swal from 'sweetalert2'
 import Toast from '../utils/toast'
 import Counter from '../components/Counter'
 import { removeFromCart } from '../actions/cartActions'
+import { CART_ACTION_RESET } from '../constants/cartConstants'
+import Message from '../components/Message'
 
 const CartScreen = (props) => {
   console.log('CartScreen render')
-  const cartItems = useSelector(state => state.cartItems)
+  const { cartItems, actionSuccess } = useSelector(state => state.cart)
   const dispatch = useDispatch()
   const onDeleteCartItemHandler = async (itemId) => {
     const result = await Swal.fire({
@@ -20,57 +23,90 @@ const CartScreen = (props) => {
     })
     if (result.isConfirmed) {
       dispatch(removeFromCart(itemId))
+    }
+  }
+
+  useEffect(() => {
+    if (actionSuccess) {
+      dispatch({ type: CART_ACTION_RESET })
       Toast.fire({
         title: '成功刪除',
         icon: 'success'
       })
     }
+  }, [dispatch, actionSuccess])
+
+
+
+  const renderCartScreen = () => {
+    if (!cartItems.length) {
+      return (
+        <Message type="info">購物車內沒有商品 <Link to="/">去選購</Link></Message>
+      )
+    }
+    const subtotal = cartItems.reduce((a, c) => a + c.price * c.quantity, 0)
+    const itemCount = cartItems.reduce((a, c) => a + c.quantity, 0)
+    return (
+      <section id="cart">
+        <div className="container">
+          <Link
+            to={{
+              pathname: props.location.state ? props.location.state.from.pathname : '/'
+            }}
+            className="nav-back-link"
+          >回到上一頁</Link>
+          <div className="cart-list-wrapper">
+            <div className="cart-header">
+              <span className="cart-img"></span>
+              <span className="cart-title">商品</span>
+              <span className="cart-price">單價</span>
+              <span className="cart-counter">數量</span>
+              <span className="cart-subtotal">總計</span>
+              <span className="cart-action">操作</span>
+            </div>
+            {
+              cartItems.map(i => (
+                <div key={i.id} className="cart-item">
+                  <img
+                    className="cart-item-img"
+                    src={i.image}
+                    alt={i.name}
+                  />
+                  <Link
+                    className="cart-item-title"
+                    to={`/products/${i.id}`}
+                  >{i.name}</Link>
+                  <span className="cart-item-price">${i.price}</span>
+                  <Counter item={i} qty={i.quantity} />
+                  <span className="cart-item-subtotal">${i.price * i.quantity}</span>
+                  <div className="cart-item-action">
+                    <button
+                      className="cart-item-delete-btn btn-link btn"
+                      onClick={() => onDeleteCartItemHandler(i.id)}
+                    >刪除</button>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+          <div className="order-placement">
+            <div className="cart-summary-subtotal">
+              <span>總金額({itemCount}項商品):</span>
+              <span className="subtotal-price">${subtotal}</span>
+            </div>
+            <Link to="/checkout" className="place-order-link">
+              <button className="place-order-btn">去買單</button>
+            </Link>
+          </div>
+        </div>
+      </section>
+    )
   }
 
-  const subtotal = cartItems.reduce((a, c) => a + c.price * c.quantity, 0)
-  const deliveryPrice = 80
-  const total = subtotal + deliveryPrice
-
   return (
-    <div className="container">
-      <Link to={{
-        pathname: props.location.state ? props.location.state.from.pathname : '/'
-      }}>回到上一頁</Link>
-      <ul>
-        {cartItems.map(item => (
-          <li key={item.id} className="cart-item-list">
-            <div className="cart-list-img-wrapper">
-              <img src={item.image} alt={item.name} />
-            </div>
-            <Link to={`/products/${item.id}`} className="card-list-title">{item.name}</Link>
-            <div className="card-list-price">$ {item.price}</div>
-            <Counter qty={item.quantity} item={item} />
-            <div className="card-list-subtotal">$ {item.quantity * item.price}</div>
-            <div className="cart-item-delete">
-              <i
-                className="far fa-trash-alt"
-                onClick={() => { onDeleteCartItemHandler(item.id) }}
-              ></i>
-            </div>
-          </li>
-        ))}
-      </ul>
-
-      <div className="cart-checkout">
-        <div className="price-subtotal">小記: {subtotal}</div>
-        <div className="price-delivery">運費: {deliveryPrice}</div>
-        <div className="price-total">總計: {total}</div>
-        {cartItems.length ? (
-          <Link to="/checkout">
-            <button type="button">買單</button>
-          </Link>
-        ) : (
-          <Link to="/">
-            <button type="button">去選購</button>
-          </Link>
-        )}
-      </div>
-    </div>
+    <>
+      {renderCartScreen()}
+    </>
   )
 }
 
